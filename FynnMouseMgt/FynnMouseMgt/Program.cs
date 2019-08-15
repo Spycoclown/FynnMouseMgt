a2de33881e165f2eb75c4e71bdfba7d4b737f4fd
Böttcher, Leonhard <leonhard.boettcher@gws.ms>
2019-08-15 08:59:32 +0200

    Mouse Speed Manipulation Integrated

--- FynnMouseMgt/FynnMouseMgt/Program.cs
+++ FynnMouseMgt/FynnMouseMgt/Program.cs
@@ -1,22 +1,66 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace FynnMouseMgt
 {
     class Program
     {
+        public const UInt32 SPI_SETMOUSESPEED = 0x0071;
+        public static bool fast = false;
+
+        [DllImport("User32.dll")]
+        static extern Boolean SystemParametersInfo(
+            UInt32 uiAction,
+            UInt32 uiParam,
+            UInt32 pvParam,
+            UInt32 fWinIni);
+
         static void Main(string[] args)
         {
 
-            //Console.WriteLine("Hit me!");
+            // Schnell machen:
             HotKeyManager.RegisterHotKey(Keys.A, KeyModifiers.Alt);
             HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressed);
+
+            // Langsam machen:
+            SlowHotKeyManager.RegisterHotKey(Keys.B, KeyModifiers.Alt);
+            SlowHotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressedSlow);
+
             Console.ReadLine();      
         }
 
         static void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
         {
-            Console.WriteLine("Hit me!");
+            if (fast)
+            {
+                return;
+            }
+            fast = true;
+
+            SystemParametersInfo(
+                SPI_SETMOUSESPEED,
+                0,
+                uint.Parse("10"),
+                0);
+            Console.WriteLine("Fast!");
+
+        }
+
+        static void HotKeyManager_HotKeyPressedSlow(object sender, HotKeyEventArgs e)
+        {
+            if(!fast)
+            {
+                return;
+            }
+            fast = false;
+            SystemParametersInfo(
+                SPI_SETMOUSESPEED,
+                0,
+                uint.Parse("5"),
+                0);
+            Console.WriteLine("Slow!");
+
         }
     }
 }
